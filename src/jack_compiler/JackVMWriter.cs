20d9369f8b99d8b881f5bf1c22db533e5f634def
Alex Johnston <me@joelj.ca>
2025-01-15 05:56:37 -0700

    Stub out JackVMWriter

---
+++ src/jack_compiler/JackVMWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jack_compiler
+{
+    internal class JackVMWriter
+    {
+        public void WritePush()
+        {
+
+        }
+
+        public void WritePop()
+        {
+
+        }
+
+        public void WriteArithmetic()
+        {
+
+        }
+
+        public void WriteLabel(string label)
+        {
+
+        }
+
+        public void WriteIf(string label)
+        {
+
+        }
+
+        public void WriteCall(string name, int nArgs)
+        {
+
+        }
+
+        public void WriteFunction(string name, int nArgs)
+        {
+
+        }
+
+        public void WriteReturn()
+        {
+
+        }
+    }
+}
