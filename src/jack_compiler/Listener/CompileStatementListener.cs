7e2f02ee755ffe58bdb121da200eac4df75ebed2
Alex Johnston <me@joelj.ca>
2025-01-16 18:54:49 -0700

    Rough stab at compiling subroutines

--- src/jack_compiler/Listener/CompileStatementListener.cs
+++ src/jack_compiler/Listener/CompileStatementListener.cs
@@ -9,7 +9,7 @@ namespace jack_compiler.Listener
 {
     internal partial class CompileListener
     {
-        public override void EnterLetStatement([NotNull] JackParserParser.LetStatementContext context)
+        public override void ExitLetStatement([NotNull] JackParserParser.LetStatementContext context)
         {
             var idx = symbolTable.IndexOf(context.ID().GetText());
             writer.WritePop(JackVMWriter.JackSegment.LOCAL, idx);
