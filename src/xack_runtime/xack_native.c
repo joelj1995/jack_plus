88dcf49adaab1c66b2dbbfcffd2cd3c708bebfa7
Alex Johnston <me@joelj.ca>
2025-01-20 08:10:20 -0700

    Reapply "Memory operations working"
    This reverts commit 9516af01cc18cf8bcac1a7b20c1418ab5af8343f.

--- src/xack_runtime/xack_native.c
+++ src/xack_runtime/xack_native.c
@@ -16,12 +16,21 @@ void native_mem_alloc()
 void native_mem_peek()
 {
     uint16_t address = pop();
-    push(vm.ram[address]);
+    int16_t result = vm.ram[address];
+    push(result);
+}
+
+void native_mem_poke()
+{
+    int16_t value = pop();
+    uint16_t address = pop();
+    vm.ram[address] = value;
 }
 
 NativeFunction native_functions[] = {
     {"Memory.alloc", native_mem_alloc},
     {"Memory.peek", native_mem_peek},
+    {"Memory.poke", native_mem_poke},
     {"", 0}
 };
 
