58f1306052d570a2a73fe91d1f1ded1edf8d46fd
Alex Johnston <me@joelj.ca>
2025-01-20 10:53:04 -0700

    Work on array and string implementation

--- src/xack_runtime/xack_native.c
+++ src/xack_runtime/xack_native.c
@@ -34,11 +34,29 @@ void native_mem_dealloc()
     push(0);
 }
 
+void native_array_new()
+{
+    native_mem_alloc();
+}
+
+void native_array_dispose()
+{
+    native_mem_dealloc();
+}
+
+void native_string_new()
+{
+    uint16_t maxLength = pop();
+}
+
 NativeFunction native_functions[] = {
     {"Memory.alloc", native_mem_alloc},
     {"Memory.peek", native_mem_peek},
     {"Memory.poke", native_mem_poke},
     {"Memory.deAlloc", native_mem_dealloc},
+    {"Array.new", native_array_new},
+    {"Array.dispose", native_array_dispose},
+    {"String.new", native_string_new},
     {"", 0}
 };
 
