e1185f694467289b1d9bc4bae99757875fad3c78
Alex Johnston <me@joelj.ca>
2025-01-20 11:39:27 -0700

    Strings maybe working

--- src/xack_runtime/xack_native.c
+++ src/xack_runtime/xack_native.c
@@ -5,6 +5,12 @@
 
 extern VM vm;
 
+typedef enum {
+    SF_MAXLENGTH,
+    SF_CURLENGTH,
+    SF_STR
+} StringFields;
+
 void native_mem_alloc()
 {
     int16_t size = pop();
@@ -46,7 +52,28 @@ void native_array_dispose()
 
 void native_string_new()
 {
+    uint16_t mem = pop();
     uint16_t maxLength = pop();
+    vm.ram[mem + SF_CURLENGTH] = 0;
+    vm.ram[mem + SF_MAXLENGTH] = maxLength;
+    if (maxLength > 0) 
+    {
+        push(maxLength);
+        native_mem_alloc();
+        vm.ram[mem + SF_STR] = pop();
+    }
+    push(mem);
+}
+
+void native_string_appendchar()
+{
+    uint16_t _this = pop();
+    uint16_t c = pop();
+    printf("Appending %c at memory location %d", c, vm.ram[_this+SF_CURLENGTH] + vm.ram[_this+SF_STR]);
+    uint16_t stringBase = vm.ram[_this+SF_CURLENGTH] + vm.ram[_this+SF_STR];
+    vm.ram[_this+SF_CURLENGTH] += 1;
+    vm.ram[stringBase] = c;
+    push(_this);
 }
 
 NativeFunction native_functions[] = {
@@ -57,6 +84,7 @@ NativeFunction native_functions[] = {
     {"Array.new", native_array_new},
     {"Array.dispose", native_array_dispose},
     {"String.new", native_string_new},
+    {"String.appendChar", native_string_appendchar},
     {"", 0}
 };
 
